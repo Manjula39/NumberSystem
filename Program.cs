e36826bafcb463b010d930aa1100f88fed856b85
Manjula36 <manjulavdt@gmail.com>
2024-02-16 09:33:10 +0530

    Add All Files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EvenOdd;
+
+Console.WriteLine("Welcome To Number System");
+Console.WriteLine("*** Even Numbers ***");
+NumberSystem.EvenNum();
+Console.WriteLine("*** Odd Numbers ***");
+NumberSystem.OddNum();
